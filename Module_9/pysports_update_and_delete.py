ede160ed1cb220ab3d5c6cfd5622bc756f7f7e0c
DHadi-5722 <92963396+DHadi-5722@users.noreply.github.com>
2021-12-03 16:33:11 -0600

    Here is my work for module 9
    In this module I have learned how to do table joins, how to insert and update table records, and how to create nested queries.

---
+++ Module_9/pysports_update_and_delete.py
@@ -0,0 +1,91 @@
+import mysql.connector
+from mysql.connector import errorcode
+config = {
+    "user": "root",
+    "password": "Flammenwerfer1994",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to continue...")
+    cursor = db.cursor()
+    cursor.execute('SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id ')
+    players= cursor.fetchall()
+    cursor.execute('INSERT INTO player (first_name, last_name, team_id) VALUES("Troy", "Birds", 1)')
+    cursor.execute('SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id ')
+    players= cursor.fetchall()
+    #cursor.execute('SELECT team_id, team_name, mascot FROM team')
+    #teams = cursor.fetchall()
+    #cursor.execute('SELECT player_id, first_name, last_name, team_id FROM player')
+    #players = cursor.fetchall()
+    #print("--DISPLAYING TEAM RECORDS--")
+    #for team in teams:
+        #print('Team ID:  {}'
+        #.format(team[0]))
+        #print('Team Name: {}'
+        #.format(team[1]))
+        #print('Mascot: {}'
+        #.format(team[2]))
+        #print()
+    print("--DISPLAYING PLAYERS AFTER INSERT--")
+    for member in players:
+        print('Player ID:  {}'
+        .format(member[0]))
+        print('First Name: {}'
+        .format(member[1]))
+        print('Last Name: {}'
+        .format(member[2]))
+        print('Team Name: {}'
+        .format(member[3]))
+        print()
+
+
+    cursor.execute('UPDATE player SET team_id = 1, first_name = "Roman", last_name = "Ferguson" WHERE first_name = "Smeagol" ')
+    cursor.execute('SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id ')
+    players= cursor.fetchall()
+         
+    print("--DISPLAYING PLAYERS AFTER UPDATE--")
+    for member in players:
+        print('Player ID:  {}'
+        .format(member[0]))
+        print('First Name: {}'
+        .format(member[1]))
+        print('Last Name: {}'
+        .format(member[2]))
+        print('Team Name: {}'
+        .format(member[3]))
+        print()
+
+
+
+    cursor.execute('DELETE FROM player WHERE first_name = "Roman"')
+    cursor.execute('SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id ')
+    players= cursor.fetchall()
+
+
+    print("--DISPLAYING PLAYERS AFTER DELETE--")
+    for member in players:
+        print('Player ID:  {}'
+        .format(member[0]))
+        print('First Name: {}'
+        .format(member[1]))
+        print('Last Name: {}'
+        .format(member[2]))
+        print('Team Name: {}'
+        .format(member[3]))
+        print()
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password is invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+        db.close()      
